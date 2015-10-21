a025fa90375d6f056a6af86745fe3e25f61d3760
sndilip1702 <sndilip17@gmail.com>
2015-10-21 10:22:18 -0700

    removed comments

--- src/hackerrankcontest/PerfectSet.java
+++ src/hackerrankcontest/PerfectSet.java
@@ -22,25 +22,7 @@ public class PerfectSet {
 				System.out.println(d>1?5:3);
 				}
 			}
-		//System.out.println(perfectSetSize(N,d));
 		in.close();
 	}
-	/*public static Long perfectSetSize(long N, long d) {
-		Map<Long, Long> numbers = new HashMap<Long,Long>();
-		long i;
-		long count = 0;
-		for(i=1;i<=N;i++){
-			numbers.put(i, i);
-		}
-		for(i=1;i<=N/2;i++){
-			if(numbers.containsKey(i) && numbers.containsKey((N+1 - i)) && i!=(N+1 - i)){
-				System.out.println(i +" "+ (N+1 - i));
-				count++;
-				numbers.remove(i);
-				numbers.remove((N+1-i));
-			}
-		}
-		return count+d;
-	}*/
 
 }
