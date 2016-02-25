73ff1190933c8f2cd3ee71ef8ba1f9b8204721e4
sndilip1702 <sndilip17@gmail.com>
2016-02-24 17:31:47 -0700

    updated String occurance for corner cases

--- src/misc/OccurStringInString.java
+++ src/misc/OccurStringInString.java
@@ -10,18 +10,21 @@ public class OccurStringInString {
         Scanner in = new Scanner(System.in);
         String text = in.nextLine();
         String toFind = in.nextLine();
-        int count = 0;
+        int count = 0, start = 0;
         if (toFind.length() <= text.length()) {
             for (int i = 0; i < text.length(); i++) {
                 int j = 0;
                 if (text.charAt(i) == toFind.charAt(j)) {
-                    while (j < toFind.length() && i<text.length() && text.charAt(i) == toFind.charAt(j)) {
+                    start = i;
+                    while (j < toFind.length() && i < text.length() && text.charAt(i) == toFind.charAt(j)) {
                         i++;
                         j++;
                     }
-                    i--; // move pointer to text back to where we want to start searching from
                     if (j == toFind.length()) {
                         count++;
+                        i--; // move pointer back to where we stopped searching
+                    } else {
+                        i = start;
                     }
                 }
             }
